942e06b6d0ec92b58b0525ab97e7d3f849ebbeac
Felipe Alexandre <felipealexandre@INV001575.local>
2025-06-30 22:41:08 -0300

    from scratch

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Configure for GitHub Pages
-config.resolver.platforms = ['web', 'native', 'ios', 'android'];
-
-module.exports = config;
